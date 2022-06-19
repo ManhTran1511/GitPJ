3d337a0b90806736dfb70ea575f1c093ab573b35
ManhTran1511 <88219084+ManhTran1511@users.noreply.github.com>
2022-06-19 19:53:30 +0700

    Add project files.

---
+++ Models/Kho.cs
@@ -0,0 +1,9 @@
+﻿namespace TestPJ.Models
+{
+    public class Kho
+    {
+        public int Id { get; set; }
+        public string TenKho { get; set; }
+        public string DiaChi { get; set; }
+    }
+}
